c42664672e1ddb24f008cd0bf4314c560956e235
GW <gustav.wickstrom01@gmail.com>
2024-08-17 13:31:28 +0200

    organisering

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function loadHTML(elementId, filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading HTML:', error);
+        });
+}
+
+// Ladda header och footer
+loadHTML('header-placeholder', 'header.html');
+loadHTML('footer-placeholder', 'footer.html');
