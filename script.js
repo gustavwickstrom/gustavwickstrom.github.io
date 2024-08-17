9a28cc73bcf32bec667fd9763d3a9eea1f859a34
GW <gustav.wickstrom01@gmail.com>
2024-08-17 12:50:10 +0200

    JS

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+console.log("Detta är ett meddelande från en extern JavaScript-fil.");
+
+// Exempel på en funktion
+function showAlert() {
+    alert("Hej! Du har precis kört en funktion från en extern JavaScript-fil.");
+}
